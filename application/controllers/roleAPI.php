b65419488a9b8a5cca42e4bcb3d598892d96ae06
Abhik <aloklata17@gmail.com>
2018-06-05 16:41:28 +0530

    Role fetching
    Role fetching

---
+++ application/controllers/roleAPI.php
@@ -0,0 +1,40 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class roleAPI extends CI_Controller{
+public function __construct()
+ {
+   parent::__construct();
+    $this->load->model('rolemastermodel','role',TRUE);
+    $this->load->library('session');
+ }
+
+ 
+ public function getrole($id=""){
+
+     $result = array();
+     if($id=="all"){
+         $data["role"]= $this->role->getActiveRole();
+         if(!empty($data["role"])){
+             $result=array(
+                 "status"=>200,
+                 "statuscode"=>"OK",
+                 "roles"=>$data["role"]
+             );
+         }else{
+             $result=array(
+                 "status"=>400,
+                 "statuscode"=>"NOK",
+                 "roles"=>"NULL"
+             );
+         }
+     } else {
+         $roleId = (int)$id;
+     }
+     
+    $json = json_encode($result);
+    header('Access-Control-Allow-Origin: *'); 
+    echo $json;
+    exit();
+     
+ }
+}
