07ef0d1099fcf9cd37210233a944664d19097c82
Abhik <aloklata17@gmail.com>
2018-06-19 18:18:25 +0530

    Xray SQL
    Xray SQL

---
+++ DB_SCRIPT/xray_center.sql
@@ -0,0 +1,45 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.5.5-10.1.31-MariaDB : Database - sish_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sish_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `sish_db`;
+
+/*Table structure for table `xray_center` */
+
+DROP TABLE IF EXISTS `xray_center`;
+
+CREATE TABLE `xray_center` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `address` text,
+  `tuid` int(10) DEFAULT NULL,
+  `lt_name` varchar(255) DEFAULT NULL,
+  `mobile_no` varchar(20) DEFAULT NULL,
+  `project_id` int(10) DEFAULT NULL,
+  `userid` int(10) DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT '1',
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_by` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+/*Data for the table `xray_center` */
+
+insert  into `xray_center`(`id`,`name`,`address`,`tuid`,`lt_name`,`mobile_no`,`project_id`,`userid`,`is_active`,`created_on`,`created_by`) values (11,'South Xray','Vill : Sukdebpur\r\nPo : Sukdebpur\r\nDist :24 PGS\r\nState:WB',1,'SHIBU','9874141566',1,1,1,'2018-06-12 14:48:08',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
