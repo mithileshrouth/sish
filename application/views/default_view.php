0dc3ef3432fbd2582d2635dd92e469b5abe41e90
shankhacse <shankhacse@gmail.com>
2018-07-10 16:57:17 +0530

    datatable

--- application/views/default_view.php
+++ application/views/default_view.php
@@ -36,7 +36,7 @@
    -->
   <!-- DataTables -->
   <link rel="stylesheet" href="<?php echo base_url();?>application/assets/diagnostic_theme/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+ <!--  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"> -->
   
   
     <!-- bootstrap wysihtml5 - text editor -->
